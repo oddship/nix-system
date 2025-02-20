f53e6a13e5906ca5e458df029e08410db7d8069b
Rohan Verma <hello@rohanverma.net>
2025-02-20 12:27:11 +0530

    fix: move old hm config here

--- home.nix
+++ home.nix
@@ -5,6 +5,33 @@
 
   home.stateVersion = "24.11";
 
+  home.packages = with pkgs; [
+    btop
+    htop
+
+    gnumake
+    fnm
+    gcc
+
+    go
+    lua
+    zig
+
+    git
+    curl
+    wget
+    ripgrep
+    fd
+    age
+    tree
+    jq
+    tmux
+    zoxide
+    eza
+    ncdu
+    websocat
+  ];
+
   # Example: Git config
   programs.git = {
     enable = true;
@@ -28,5 +55,51 @@
   };
 
   # Zsh
-  programs.zsh.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "docker" "ssh-agent" "dotenv" ];
+        theme = "robbyrussell";
+      };
+      shellAliases = {
+        ls = "eza --icons=always";
+        cd = "z";
+        sudo = "sudo --preserve-env=PATH env";
+      };
+      initExtra = ''
+        export PATH=$HOME/go/bin:$PATH
+        # zoxide integration = for z 
+        eval "$(zoxide init zsh)"
+        # fnm integration for node js
+        eval "$(fnm env --use-on-cd)"
+        # ssh-agent config
+        zstyle :omz:plugins:ssh-agent lazy yes
+        zstyle :omz:plugins:ssh-agent identities id_ed25519
+        zstyle :omz:plugins:ssh-agent lifetime 1h
+      '';
+    };
+
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      extraConfig = ''
+        set number
+        set relativenumber
+        set expandtab
+        set tabstop=2
+        set shiftwidth=2
+      '';
+    };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
 }
