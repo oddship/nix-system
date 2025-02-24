cdcfaed5bb73d18bbed99acfbded49321ab1a222
Rohan Verma <hello@rohanverma.net>
2025-02-25 00:33:34 +0530

    feat: switch to gnome

--- configuration.nix
+++ configuration.nix
@@ -62,9 +62,60 @@
   ################################
 
   services.xserver.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = with pkgs; [
+    orca
+    evince
+    # file-roller
+    geary
+    gnome-disk-utility
+    # seahorse
+    # sushi
+    # sysprof
+    #
+    # gnome-shell-extensions
+    #
+    # adwaita-icon-theme
+    # nixos-background-info
+    gnome-backgrounds
+    # gnome-bluetooth
+    # gnome-color-manager
+    # gnome-control-center
+    # gnome-shell-extensions
+    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    gnome-user-docs
+    # glib # for gsettings program
+    # gnome-menus
+    # gtk3.out # for gtk-launch program
+    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
+    # xdg-user-dirs-gtk # Used to create the default bookmarks
+    #
+    baobab
+    epiphany
+    gnome-text-editor
+    # gnome-calculator
+    # gnome-calendar
+    gnome-characters
+    # gnome-clocks
+    gnome-console
+    # gnome-contacts
+    # gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    # gnome-system-monitor
+    gnome-weather
+    # loupe
+    # nautilus
+    # gnome-connections
+    simple-scan
+    snapshot
+    totem
+    yelp
+    gnome-software
+  ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
@@ -171,12 +222,14 @@
     wl-clipboard
 
     syncthing
-    
+
     tail-tray
 
-    obsidian
+    gnome-tweaks
+    gnome-extension-manager
+    bibata-cursors
 
-    kdePackages.yakuake
+    obsidian
 
     # fonts
     fira-code
@@ -195,7 +248,7 @@
   services.syncthing = {
     enable = true;
     user = "rhnvrm";
-    dataDir = "/home/rhnvrm"; #TODO this can be made to come form config
+    dataDir = "/home/rhnvrm"; # TODO this can be made to come form config
     # TODO: setup the initial devices from a secrets store.
   };
 
