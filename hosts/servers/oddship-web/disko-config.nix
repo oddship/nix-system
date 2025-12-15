026b414dafefda5c836d1a528fb7b5d5ca2bf5d7
Rohan Verma <hello@rohanverma.net>
2025-12-15 13:57:22 +0530

    fix: use exact LGUG2Z working disko config for Hetzner
    Change disk name from 'main' to 'disk1' to match proven working config.
    This affects partition label generation.
    
    Based on: https://github.com/LGUG2Z/nixos-hetzner-cloud-starter

--- hosts/servers/oddship-web/disko-config.nix
+++ hosts/servers/oddship-web/disko-config.nix
@@ -1,36 +1,35 @@
 { lib, ... }:
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = lib.mkDefault "/dev/sda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF02";
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-            ESP = {
-              name = "ESP";
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              name = "root";
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
             };
           };
         };
